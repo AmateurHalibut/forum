1feda5b0b321e80ee5f24b294e33703c0d6ae40e
AmateurHalibut <crashal96@gmail.com>
2021-04-23 13:56:49 -0400

    More changes for database

--- server/index.js
+++ server/index.js
@@ -2,15 +2,10 @@
 
 const path = require("path");
 const express = require("express");
-const mysql = require("mysql");
+//const mysql = require("mysql");
 
 // Create connection to DB
-const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'CS3141',
-    database: 'cs3141',
-});
+const db = require('./models')
 
 const PORT = process.env.PORT || 3001;
 
@@ -20,25 +15,22 @@ const app = express();
 /*app.use(express.static(path.resolve(__dirname, '../client/build')));*/
 
 // Handle GET requests to /api route
-//app.get("/api", (req, res) => {
-//  res.json({ message: "Hello from server!" });
-//});
-
 app.get("/api", (req, res) => {
-    const sqlInsert = "insert into user_acc (email, pswd, date_created) values('anhromad@aol.edu', 'stuff', NOW());"
-    db.query(sqlInsert, (err, result) => {
-        res.json({ message: "Successfully inserted into database!" });
-    })
+  res.json({ message: "Hello from server!" });
 });
+
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
+})
 
 // All other GET requests not handled before will return our React app
 //app.get('*', (req, res) => {
 //  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 //});
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+
 
 
 
